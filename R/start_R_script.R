dab7c30c5d825dfe5b099adcee49092e8b9e082e
Simon Schulte <s.pedro@gmx.de>
2020-04-17 11:32:33 +0200

    minor change to start_R_script

--- R/start_R_script.R
+++ R/start_R_script.R
@@ -17,6 +17,7 @@ start_R_script <- function(filename = "Untitled.R",
                            packages = TRUE) { 
   
   path2template <- system.file(file.path("extdata", "template.R"), package = "my.utils")
+  if (substr_right(filename,2) != ".R") filename <- paste0(filename, ".R") 
   new.path <- file.path(dir, filename)
   # write to template
   template_out <- whisker::whisker.render(template = readLines(path2template),
